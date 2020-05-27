3e5519d5fd538d33265fa3a471c6bdfe28465cd6
Marc <g40419@gmail.com>
2020-05-27 22:22:56 +0800

    feat: add express-handlebars & 模板

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout : 'main' }))
 app.set('view engine', 'handlebars')
 
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
 app.listen( 3000, () => {
   console.log('now is running')
 })
